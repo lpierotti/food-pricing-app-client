62179deb127097b3a5b1ef11d5860319b59f9689
Caitlin Keck <caitlin.keck@gmail.com>
2017-09-11 23:16:25 -0400

    added some more filtering and made logo a link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Route} from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
 import RecipeSearch from './components/RecipeSearch'
 import RestaurantSearch from './components/RestaurantSearch'
 import Menu from './components/Menu'
@@ -11,9 +11,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>Butter Grams</h1>
+          <h1><Link to={`/`}>Butter Grams</Link></h1>
         </div>
-         
         <Route exact path={'/'} component={RestaurantSearch}/>
         <Route path={'/menus/:id'} render={(match) => <Menu venueId={match} />}/>
       </div>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -24,7 +24,7 @@ class Menu extends React.Component {
     // console.log(this.state.menu.menu)
     return (
       <div className="menu-items">
-        {this.state.menu.menu ? this.state.menu.menu.map((type,index) => <div className="menuType" key={index}><h1>{type.name}</h1><p>{type.description}</p>{type.entries.items.map((item,index) => <div key={index}><Item data={item} description={item} price={item}/></div>)}</div>) : null}
+        {this.state.menu.menu ? this.state.menu.menu.map((type,index) => <div className="menuType" key={index}><h1>{type.name}</h1><p>{type.description}</p>{type.entries.items.map((item,index) => <div key={index}><Item data={item} description={item} price={item}/></div>)}</div>) : <h1>Sorry, but this restaurant does not have an available menu.</h1>}
       </div>
     )
   }
